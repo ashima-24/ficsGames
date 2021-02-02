90e821aeb83c61896c28828ccec334603b83022f
Ashima <s8asjind@stud.uni-saarland.de>
2021-02-02 01:21:24 +0100

    memory issue solved for now

--- games.cpp
+++ games.cpp
@@ -2,41 +2,37 @@
 #include<vector>
 #include<fstream>
 #include<string>
-#include<map>
 #include<utility>
 
 
 using namespace std;
 
-int main()
+vector<string>colHead;
+vector<pair<size_t , pair<string, string>>>colVal;
+
+size_t counter = 0;
+vector<size_t> count;
+string tempHead = "";
+string tempVal = "";
+
+bool flag = false;
+
+//ifstream inputFile("ficsgamesdb_2020_chess_nomovetimes_184120.pgn");
+ifstream inputFile("test.pgn");
+ifstream inputFile2("test.pgn");
+//	ifstream inputFile("tail.pgn");
+ofstream outFile("out.csv" ,ios::app);
+
+void ficsGamesHeaders()
 {
-	vector<string>colHead;
-	
-	vector<pair<size_t , pair<string, string>>>colVal;
-	//vector<pair<string, string>>colVal;
-	//map<string, string>colVal;
-
-	size_t counter = 0;
-	vector<size_t> count;
-
-	string tempHead = "";
-	string tempVal = "";
-
-	bool flag = false;
-
-	ifstream inputFile("ficsgamesdb_2020_chess_nomovetimes_184120.pgn");
-//	ifstream inputFile("test.pgn");
-	//	ifstream inputFile("tail.pgn");
-	ofstream outFile("out.csv" ,ios::app);
 
 	if (inputFile.is_open()) 
 	{
 		string line;
 		while (getline(inputFile, line)) 
 		{
-
+		
 			size_t start = 0;
-
 
 			if(line[start] == '[')
 			{
@@ -69,6 +65,7 @@ int main()
 					{
 						colHead.push_back(tempHead);
 					}
+
 					if(tempHead == "Event")
 					{	
 						++counter;
@@ -84,9 +81,6 @@ int main()
 						++start;
 					}
 
-					//colVal.insert(pair<string, string> (tempHead, tempVal));
-					colVal.push_back(make_pair(counter,  make_pair(tempHead, tempVal)));
-					//colVal.push_back(make_pair(tempHead, tempVal));
 
 					break;
 				}
@@ -97,7 +91,6 @@ int main()
 	}
 	inputFile.close();
 
-	bool flag2 = false;
 
 	for(size_t i = 0; i != colHead.size(); ++i)
 	{
@@ -105,38 +98,110 @@ int main()
 		outFile<<colHead[i]<<',';
 
 	}
-	outFile<<"\n";
-
-for(size_t k = 1; k<= count.size(); ++k)
-{
-	for(size_t j = 0; j != colHead.size(); ++j)
-	{
-		for(size_t i = 0; i != colVal.size()  ; ++i)
-		{
-			if((colVal[i].first == k) && (colHead[j].compare(colVal[i].second.first) == 0))
-			{
-				outFile<<colVal[i].second.second<<',';
-				flag2 = false;
-				
-				break;
-			}
-			else
-			{
-				flag2 = true;
-			}
-			
-		}
-		if(flag2)
-		{
-			//cout<<"here";
-		outFile<<"null"<<",";
-		}
-	}
-	if(true)
-	outFile<<"\n";
-
+	//outFile<<"\n";
 }
 
+void ficsGamesValues()
+{
+	bool flag2 = false;
+	bool update = false;
+
+	size_t counter = 0;
+
+	vector<pair<size_t, string>>indivHead;
+	if (inputFile2.is_open()) 
+	{
+		string line;
+		while (getline(inputFile2, line)) 
+		{
+
+			size_t start = 0;
+
+			if(line[start] == '[')
+			{
+				tempHead = "" ;	
+				tempVal = "" ;
+
+				for(size_t start = 1; start< line.length(); ++start)
+				{ 
+
+					while(line[start] != ' ')
+					{	
+						tempHead += line[start];
+						++start;
+					}
+					++start;
+
+					while(line[start] != ']')
+
+					{
+						tempVal += line[start];
+
+						++start;
+					}
+
+					if(tempHead == "Event")
+					{	 
+						++counter;
+						outFile<<"\n";
+						colVal.clear();
+		
+					}	
+
+					colVal.push_back(make_pair(counter,  make_pair(tempHead, tempVal)));
+
+					if(true)
+					{
+						for(size_t k = counter; k<= counter; ++k)
+						{
+							for(size_t j = 0; j != colHead.size(); ++j)
+							{
+								for(size_t i = 0; i != colVal.size()  ; ++i)
+								{
+									if((colVal[i].first == k) && (colHead[j].compare(colVal[i].second.first) == 0))
+									{
+										outFile<<colVal[i].second.second<<',';
+										flag2 = false;
+
+										break;
+									}
+									else
+									{
+										flag2 = true;
+									}
+
+								}
+								if(flag2)
+								{
+								
+									outFile<<"null"<<",";
+								}
+							}
+							if(true)
+								outFile<<"\n";
+
+						}
+						
+						//count.push_back(counter);
+					}
+					//colVal.insert(pair<string, string> (tempHead, tempVal));
+					//colVal.push_back(make_pair(counter, tempHead ));
+
+					break;
+
+				}
+				
+			}
+		}
+
+	}
+	inputFile2.close();
+
+}
+int main()
+{
+	ficsGamesHeaders();
+	ficsGamesValues();
 
 }
 
